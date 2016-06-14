ca5c41228ed01a54998f8bf4988450ba8b4ccbf7
Csiki Dávid <csdivad@gmail.com>
2016-06-14 22:22:29 +0200

    added enter key handling

--- src/main/java/hu/csdivad/xy/vaadin/view/LoginView.java
+++ src/main/java/hu/csdivad/xy/vaadin/view/LoginView.java
@@ -5,10 +5,17 @@ import org.vaadin.risto.formsender.widgetset.client.shared.Method;
 
 import com.ejt.vaadin.loginform.LoginForm;
 import com.vaadin.data.validator.NullValidator;
+import com.vaadin.event.Action;
+import com.vaadin.event.FieldEvents.FocusEvent;
+import com.vaadin.event.FieldEvents.FocusListener;
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.event.ShortcutListener;
+import com.vaadin.event.ShortcutAction.KeyCode;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.AbstractComponent;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
@@ -26,6 +33,7 @@ public class LoginView extends LoginForm implements View {
 	public static final String VIEW_NAME = "login";
 
 	private TextField accountNumberField = new TextField();
+	Action action_ok = new ShortcutAction("Default key", ShortcutAction.KeyCode.ENTER, null);
 
 	@Override
 	protected Component createContent(TextField userNameField, PasswordField passwordField, Button loginButton) {
@@ -57,26 +65,39 @@ public class LoginView extends LoginForm implements View {
 
 	}
 
-	private Component createLoginDetailsForm(TextField username, PasswordField password, Button login) {
+	private Component createLoginDetailsForm(TextField usernameField, PasswordField passwordField, Button loginButton) {
 		FormLayout loginDetailsForm = new FormLayout();
 		loginDetailsForm.setSpacing(true);
 		loginDetailsForm.setMargin(true);
 		loginDetailsForm.setSizeUndefined();
 
-		username.setCaption("Ügyfélazonosító");
-		username.setIcon(FontAwesome.USER);
-		username.addValidator(new NullValidator("Hiányzó ügyfélazonosító", false));
+		usernameField.setCaption("Ügyfélazonosító");
+		usernameField.setIcon(FontAwesome.USER);
+		usernameField.addValidator(new NullValidator("Hiányzó ügyfélazonosító", false));
 
-		password.setCaption("Jelszó");
-		password.setIcon(FontAwesome.LOCK);
-		password.addValidator(new NullValidator("Hiányzó jelszó", false));
+		passwordField.setCaption("Jelszó");
+		passwordField.setIcon(FontAwesome.LOCK);
+		passwordField.addValidator(new NullValidator("Hiányzó jelszó", false));
 
 		accountNumberField.setCaption("Számlaszám");
 		accountNumberField.setIcon(FontAwesome.BOOK);
 		accountNumberField.addValidator(new NullValidator("Hiányzó számlaszám", false));
 
-		loginDetailsForm.addComponents(username, password, accountNumberField, login);
+		registerEnterKey(accountNumberField, usernameField, passwordField);
+		loginButton.setCaption("Bejelentkezés");
+		loginDetailsForm.addComponents(usernameField, passwordField, accountNumberField, loginButton);
 		return loginDetailsForm;
+	}
+
+	private void registerEnterKey(AbstractComponent component, TextField usernameField, PasswordField passwordField) {
+		component.addShortcutListener(new ShortcutListener("Enter", ShortcutAction.KeyCode.ENTER, null) {
+			private static final long serialVersionUID = -1029061321445408387L;
+
+			@Override
+			public void handleAction(Object sender, Object target) {
+				LoginView.this.login(usernameField.getValue(), passwordField.getValue());
+			}
+		});
 	}
 
 	private Component createAdditionalMsgs() {
